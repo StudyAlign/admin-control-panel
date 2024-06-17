e7ddca18c8d6bb9618f3f240660426adf8dd8ccd
Jannek <janneksekowski@gmail.com>
2024-06-17 19:21:43 +0200

    - [Added] Edit Study Information Page

--- src/app/create_study_flow/EditInformation.js
+++ src/app/create_study_flow/EditInformation.js
@@ -1,21 +1,28 @@
-import React, {useState} from "react";
-import StudyCreationLayout, {CreationSteps} from "./StudyCreationLayout";
-import {Button, Col, Row, Container, Form} from "react-bootstrap";
-import {useDispatch, useSelector} from "react-redux";
-import {useAuth} from "../../components/Auth";
-import {createStudy, selectStudy} from "../../redux/reducers/studySlice";
-import {useNavigate} from "react-router";
+import React, { useState, useEffect } from "react";
+import { Button, Col, Row, Container, Form } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router";
+
+import {
+    updateStudy,
+    selectStudy,
+    selectStudySetupInfo,
+    getStudy,
+    getStudySetupInfo
+} from "../../redux/reducers/studySlice";
+
 import LoadingScreen from "../../components/LoadingScreen";
-import {Navigate, useLocation} from "react-router-dom";
+import StudyCreationLayout, { CreationSteps, StudyStatus } from "./StudyCreationLayout";
 
 export default function EditInformation(props) {
 
     const { status } = props
 
+    const { study_id } = useParams()
     const dispatch = useDispatch()
-    const auth = useAuth()
-    const location = useLocation()
+    const navigate = useNavigate()
 
+    const [disabled] = useState(status === StudyStatus.Active ? true : false)
     const [title, setTitle] = useState('')
     const [startDate, setStartDate] = useState('')
     const [endDate, setEndDate] = useState('')
@@ -23,9 +30,28 @@ export default function EditInformation(props) {
     const [description, setDescription] = useState('')
     const [consent, setConsent] = useState('')
 
-    const [created, setCreated] = useState(false)
-
     const study = useSelector(selectStudy)
+    const studySetupInfo = useSelector(selectStudySetupInfo)
+
+    useEffect(() => {
+        dispatch(getStudy(study_id))
+        dispatch(getStudySetupInfo(study_id))
+    }, [])
+
+    useEffect(() => {
+        if (study && studySetupInfo) {
+            setTitle(study.name)
+            setStartDate(study.startDate.split('T')[0])
+            setEndDate(study.endDate.split('T')[0])
+            setAmountParticipants(studySetupInfo.planned_number_participants)
+            setDescription(study.description)
+            setConsent(study.consent)
+        }
+    }, [study, studySetupInfo])
+
+    if (study == null || studySetupInfo == null) {
+        return <LoadingScreen />
+    }
 
     const handleTitle = (event) => {
         setTitle(event.target.value)
@@ -53,30 +79,23 @@ export default function EditInformation(props) {
 
     const handleSubmit = async (event) => {
         event.preventDefault()
-        let study = {
-            "name": title,
-            "startDate": startDate + "T15:08:50.161Z",
-            "endDate": endDate  + "T15:08:50.161Z",
-            "is_active": false, // TODO how to initialize study? As active or not active
-            "owner_id": auth.user.id,
-            "invite_only": false, // TODO how to indicate if invite_only or not? Checkbox?
-            "description": description,
-            "consent": consent,
-            "planned_number_participants": amountParticipants,
-            "planned_procedure": null,
-            "current_setup_step": "study"
-        }
-        await dispatch(createStudy(study))
-        setCreated(true)
-    }
-
-    if (created) {
-        if (study == null) {
-            return <LoadingScreen/>
-        }
-        else {
-            return <Navigate to={"/create/" + study.id + "/procedure"} replace state={{ from: location }}/>
-        }
+        await dispatch(updateStudy({
+            "studyId": study_id,
+            "study": {
+                "name": title,
+                "startDate": startDate + "T15:08:50.161Z",
+                "endDate": endDate + "T15:08:50.161Z",
+                "is_active": false, // TODO how to initialize study? As active or not active
+                "invite_only": false, // TODO how to indicate if invite_only or not? Checkbox?
+                "description": description,
+                "consent": consent,
+                "planned_number_participants": amountParticipants,
+                "planned_procedure": null,
+                "current_setup_step": "study" // TODO change to "information" after backend change
+            }
+        }))
+        await dispatch(getStudySetupInfo(study_id))
+        navigate("/create/" + study_id + "/procedure")
     }
 
     return(
@@ -87,7 +106,7 @@ export default function EditInformation(props) {
                     <Row>
                         <Form.Group className="mb-3" controlId="formTitle">
                             <Form.Label>Title</Form.Label>
-                            <Form.Control required type="text" placeholder="Title" value={title} onChange={handleTitle}/>
+                            <Form.Control disabled={disabled} required type="text" placeholder="Title" value={title} onChange={handleTitle}/>
                         </Form.Group>
                     </Row>
 
@@ -95,13 +114,13 @@ export default function EditInformation(props) {
                         <Col>
                             <Form.Group className="mb-3" controlId="formStartDate">
                                 <Form.Label>Start Date</Form.Label>
-                                <Form.Control required type="date" placeholder="Start Date" value={startDate} onChange={handleStartDate}/>
+                                <Form.Control disabled={disabled} required type="date" placeholder="Start Date" value={startDate} onChange={handleStartDate}/>
                             </Form.Group>
                         </Col>
                         <Col>
                             <Form.Group className="mb-3" controlId="formEndDate">
                                 <Form.Label>End Date</Form.Label>
-                                <Form.Control required type="date" placeholder="End Date" value={endDate} onChange={handleEndDate}/>
+                                <Form.Control disabled={disabled} required type="date" placeholder="End Date" value={endDate} onChange={handleEndDate}/>
                             </Form.Group>
                         </Col>
                     </Row>
@@ -128,7 +147,7 @@ export default function EditInformation(props) {
                     </Row>
 
                     <Row>
-                        <Col> <Button type="submit" size="lg">Save and Proceed</Button> </Col>
+                        <Col> <Button type="submit" size="lg">Update and Proceed</Button> </Col>
                     </Row>
                 </Form>
             </Container>
