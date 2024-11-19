774dda16a11399b76051447bbcee3e083119fb09
Jannek <janneksekowski@gmail.com>
2024-11-19 23:48:51 +0100

    [Add] Env link and ckeditor style

--- src/app/study_overview/Overview.js
+++ src/app/study_overview/Overview.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Button } from "react-bootstrap";
+import { Button, Tabs, Tab } from "react-bootstrap";
 import { useDispatch} from "react-redux";
 
 import { STATES } from "./StudyOverviewLayout";
@@ -8,15 +8,20 @@ import { studySlice, getStudySetupInfo, getStudy, updateStudy } from "../../redu
 
 import { reformatDate } from "../../components/CommonFunctions";
 
+import { Copy, Check } from "react-bootstrap-icons";
+
 import "./StudyOverview.css";
 import "../SidebarAndReactStyles.scss";
+
+const url = process.env.REACT_APP_STUDY_ALIGN_STUDY_FRONTEND_URL;
 
 export default function Overview(props) {
     const [participants, setParticipants] = useState("loading...")
     const [doneParticipants, setDoneParticipants] = useState("loading...")
-
+    const [isClicked, setIsClicked] = useState(false)
     const dispatch = useDispatch()
     const study = props.study
+    const collab_url = url + study.id + "/"
 
     useEffect(async () => {
         await dispatch(getStudySetupInfo(study.id)).then((response) => {
@@ -26,6 +31,12 @@ export default function Overview(props) {
             dispatch(studySlice.actions.resetStudySetupInfo())
         })
     }, [])
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(collab_url);
+        setIsClicked(true);
+        setTimeout(() => setIsClicked(false), 500); // Rücksetzen nach 500ms
+    }
 
     const handleStudyActive = async (event, state) => {
         event.preventDefault()
@@ -59,40 +70,54 @@ export default function Overview(props) {
         }
     }
 
-    return(
+    return (
         <>
-            <table>
-                <tbody>
-                <tr>
-                    <td className="content-name"> Description: </td>
-                    <td> {study.description} </td>
-                </tr>
-                <tr>
-                    <td className="content-name"> Consent: </td>
-                    <td> {study.consent} </td>
-                </tr>
-                <tr>
-                    <td className="content-name"> Status: </td>
-                    <td> {getStatus()} {getStudyIsOver()} </td>
-                </tr>
-                <tr>
-                    <td className="content-name"> Start Date: </td>
-                    <td> {reformatDate(study.startDate)} </td>
-                </tr>
-                <tr>
-                    <td className="content-name"> End Date: </td>
-                    <td> {reformatDate(study.endDate)} </td>
-                </tr>
-                <tr>
-                    <td className="content-name"> Participants: </td>
-                    <td> {doneParticipants} / {participants} </td>
-                </tr>
-                <tr>
-                    <td className="content-name"> Invite Link: </td>
-                    <td> <a href={""}> N.A. </a> </td>
-                </tr>
-                </tbody>
-            </table>
+            <Tabs defaultActiveKey="details" id="study-tabs" className="mb-3">
+                <Tab eventKey="details" title="Details">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td className="content-name"> Status: </td>
+                                <td> {getStatus()} {getStudyIsOver()} </td>
+                            </tr>
+                            <tr>
+                                <td className="content-name"> Start Date: </td>
+                                <td> {reformatDate(study.startDate)} </td>
+                            </tr>
+                            <tr>
+                                <td className="content-name"> End Date: </td>
+                                <td> {reformatDate(study.endDate)} </td>
+                            </tr>
+                            <tr>
+                                <td className="content-name"> Participants: </td>
+                                <td> {doneParticipants} / {participants} </td>
+                            </tr>
+                            <tr>
+                                <td className="content-name"> Invite Link: </td>
+                                <td> <a href={collab_url}> {collab_url} </a> <button
+                                    onClick={handleCopy}
+                                    className={`copy-button ${isClicked ? 'clicked' : ''}`}
+                                >
+                                    <Copy size={10} />
+                                </button> </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </Tab>
+                <Tab eventKey="description" title="Description">
+                    <div
+                        className="description-content"
+                        dangerouslySetInnerHTML={{ __html: study.description }}>
+                    </div>
+                </Tab>
+                <Tab eventKey="consent" title="Consent">
+                    <div
+                        className="consent-content"
+                        dangerouslySetInnerHTML={{ __html: study.consent }}>
+                    </div>
+                </Tab>
+            </Tabs>
+            <hr />
             {getButton(study.state)}
             <hr />
             <h3> Collaborators </h3>
